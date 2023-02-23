7ff4a9f7ddff0fd9d67d2dbae302af9ac1cee554
Jake <calcuttj@msu.edu>
2023-02-22 16:07:25 -0800

    Updates including training

--- pdsp_resnet_pt.py
+++ pdsp_resnet_pt.py
@@ -6,12 +6,39 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 from collections import OrderedDict as ODict
+import numpy as np
 device = 'cpu'
 
 """
 Utility function for computing output of convolutions
 takes a tuple of (h,w) and returns a tuple of (h,w)
 """
+
+def calc_f1(pred, target):
+  pred_max = np.argmax(pred, axis=1)
+
+  f1 = 0.
+
+  cats = [
+    [(pred_max == i) & (target == i), (pred_max == i) & (target != i), (pred_max != i) & (target == i)]
+    for i in range(4)
+  ]
+
+  print(cats)
+
+  for i in range(4):
+
+    tp_i = sum(cats[i][0])
+    fp_i = sum(cats[i][1])
+    fn_i = sum(cats[i][2])
+
+    print(f'TP_{i}', tp_i)
+    print(f'FP_{i}', fp_i)
+    print(f'FN_{i}', fn_i)
+
+    f1 += .25 * (tp_i)/(tp_i + .5*(fp_i + fn_i))
+  return f1
+
 def conv_output_shape(h_w, kernel_size=1, stride=1, pad=0, dilation=1):
     from math import floor
     if type(kernel_size) is not tuple:
@@ -33,11 +60,11 @@ class Stage1Block(nn.Module):
     self.layers = nn.Sequential(*self.blocks)
 
   def forward(self, x):
-    print('\tStage1')
-    #return self.layers(x)
-    for b in self.blocks:
-      x = b(x)
-    return x
+    #print('\tStage1')
+    return self.layers(x)
+    #for b in self.blocks:
+    #  x = b(x)
+    #return x
 
 class NBlock(nn.Module):
   def __init__(self, depth, insize, infilters, outfilters, ksize=3):
@@ -112,21 +139,21 @@ class Stage2NBlock(nn.Module):
   def forward(self, x):
     import os, psutil;
     process = psutil.Process(os.getpid())
-    print('\tStage2N')
-    print(process.memory_info().rss*1e-9)
+    #print('\tStage2N')
+    #print(process.memory_info().rss*1e-9)
     #y = self.layers(x)
     y = self.blocks[0](x)
-    print(0, process.memory_info().rss*1e-9)
+    #print(0, process.memory_info().rss*1e-9)
     for i,b in enumerate(self.blocks[1:]):
       y = b(y)
-      print(i, process.memory_info().rss*1e-9)
-    print(process.memory_info().rss*1e-9)
+      #print(i, process.memory_info().rss*1e-9)
+    #print(process.memory_info().rss*1e-9)
 
     if self.increased_filters:
       y = torch.add(self.conv_1x1(x), y)
     else:
       y = torch.add(x, y)
-    print(process.memory_info().rss*1e-9)
+    #print(process.memory_info().rss*1e-9)
 
     return y
 
@@ -226,47 +253,167 @@ class SEResnetPDSP(nn.Module):
 
     return y 
 
-def load_data(files):
-  pdsp_datas = [process_hits.PDSPData(f) for f in files]
-  planes = [[] for i in range(3)]
-  truths = [] 
 
-  a = 0 
-  for pd in pdsp_datas:
-    print(a, end='\r')
-    a += 1
-    pd.load_truth()
-    for i, t in zip(range(pd.nevents), pd.topos):
-      if t < 0: continue
+class SEResnetPDSP_Plane2(nn.Module):
+  def __init__(self, depth=[4, 6, 3], nfilters=[225, 256, 512],
+               ntimes=500, nwires=480):
+    super().__init__()
 
-      truths.append(t)
-      pd.get_event(i)
+    self.nwires = nwires
+    #1 stage1 block for each
+    self.stage1 = Stage1Block()
 
-      for j in range(3):
-        planes[j].append(pd.make_plane(j, pad2=True))
+    self.stage1_shape = conv_output_shape(
+        conv_output_shape((ntimes, nwires), kernel_size=7, stride=2),
+        kernel_size=3, stride=2)
 
-  return (planes, truths)
+    #3 stage 2 blocks for each branch
+    #do not increase filters here
+    self.stage2 = NBlock(3, self.stage1_shape, 64, 64)
+
+    self.stage1_2_layers = nn.Sequential(self.stage1, self.stage2)
+
+    ##use plane 0 -- plane 2 will be padded to its size
+    self.stage3n_shapes = [self.stage1_shape]
+    for i in range(1, len(depth)):
+      self.stage3n_shapes.append(conv_output_shape(self.stage3n_shapes[i-1], kernel_size=3, stride=2, pad=1))
+
+    final_shape = conv_output_shape(self.stage3n_shapes[-1], kernel_size=3, stride=2, pad=1)
+
+    full_nfilters = [64, *nfilters]
+    self.stage3n = [
+      NBlock(depth[i], self.stage3n_shapes[i], full_nfilters[i], full_nfilters[i+1])
+      for i in range(len(depth))
+    ]
+    self.stage3_layers = nn.Sequential(*self.stage3n)
+
+    self.final_layers = nn.Sequential(
+      #batch norm
+      nn.BatchNorm2d(nfilters[-1]),
+      #relu
+      nn.ReLU(),
+      #global avg pool
+      nn.AvgPool2d(final_shape),
+      #linear
+      nn.Flatten(),
+      nn.Linear(nfilters[-1] ,4),
+      #softmax
+      nn.Softmax()
+    )
+
+  def forward(self, x):
+
+    #plane0_in = x[:, 0, :, :].reshape(x.shape[0], 1, *x.shape[2:])
+    #plane1_in = x[:, 1, :, :].reshape(x.shape[0], 1, *x.shape[2:])
+    #plane2_in = x[:, 2, :, p2_start_initial:-p2_start_initial]
+    #plane2_in = plane2_in.reshape(x.shape[0], 1, *plane2_in.shape[1:])
+
+    #planes = [plane0_in, plane1_in, plane2_in]
+    #print(plane0_in.shape)
+
+    y = self.stage1_2_layers(x)
+    y = self.stage3_layers(y)
+    y = self.final_layers(y)
+
+    '''print('Stages 1 & 2')
+    stage1_outs = []
+    for layer,x in zip(self.stage1_2_layers, planes):
+      stage1_outs.append(layer(x))
+
+    print('Done')
+
+    #y = self.stage1_2_layers[2](x)
+
+    print('Padding')
+    stage1_p2_pad = torch.zeros(x.shape[0], 64, *self.stage1_shapes[0])
+    print(stage1_p2_pad.shape)
+    p2_start = (self.stage1_shapes[0][1] - self.stage1_shapes[2][1])//2
+
+    stage1_p2_pad[:, :, :, p2_start:-p2_start] = stage1_outs[2]
+    stage1_outs = [stage1_outs[0], stage1_outs[1], stage1_p2_pad]
+    y = torch.cat(stage1_outs, 1)
+
+    print(y.shape)
+    y = self.stage3_layers(y)
+
+    y = self.final_layers(y)'''
+
+    return y 
 
 
-def train(model, loss_fn, optimizer, pdsp_data):
+def train(model, loss_fn, optimizer, pdsp_data, batchsize=32, plane2=False, epochs=1, validate=False, maxbatches=-1):
 
-  if not pdsp_data.loaded_truth: pdsp_data.load_truth()
-  pdsp_data.clean_events()
+  import matplotlib.pyplot as plt
+  import numpy as np
+  from IPython import display
+  #%matplotlib inline
+  
+  if not pdsp_data.loaded_truth:
+    pdsp_data.load_truth()
+    pdsp_data.clean_events()
 
-  for batch, (X, y) in enumerate(
-      pdsp_data.get_training_batches(batchsize=32)):
-    #X, y = X.to(device), y.to(device)
+  losses = []
+  epochal_avg_loss = []
+  validation_losses = []
+  for e in range(epochs):
+    print('Start epoch', e)
+    losses.append([])
+    running_losses = []
+    epochal_avg_loss.append(0.)
 
-    # Compute prediction error
-    pred = model(torch.from_numpy(X).float())
-    loss = loss_fn(pred, y)
 
-    # Backpropagation
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    for batch, (X, y) in enumerate(
+        pdsp_data.get_training_batches(batchsize=batchsize, plane2_only=plane2, maxbatches=maxbatches)):
+      #X, y = X.to(device), y.to(device)
 
-    if batch % 100 == 0:
+      # Compute prediction error
+      optimizer.zero_grad()
+      pred = model(torch.from_numpy(X).float())
+      loss = loss_fn(pred, torch.from_numpy(y).float())
+
+      # Backpropagation
+      loss.backward()
+      optimizer.step()
+
+      #if batch % 100 == 0:
       loss, current = loss.item(), batch * len(X)
-      print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+      losses[-1].append(loss)
+      running_losses.append(np.mean(losses[-1][-5:]))
+      epochal_avg_loss[-1] += loss
 
+      batch_f1 = 0.
+
+      try:
+        plt.close()
+        plt.plot(losses[-1])
+        plt.plot(running_losses)
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+        #plt.close()
+        time.sleep(1)
+      except KeyboardInterrupt:
+        break
+
+      print(f"loss: {loss:>7f}  [{current:>5d}/{pdsp_data.nevents}]")
+      #for i, j in zip(pred, y):
+      #  print(i,j)
+    epochal_avg_loss[-1] /= len(losses[-1])
+    print(f'Epoch Loss: {epochal_avg_loss[-1]:>7f}')
+
+    #validation step
+    if not validate: continue
+    validation_losses.append(0.)
+    n_valid = 0
+    print('Validation')
+    startbatch = int(.9*pdsp_data.get_nbatches(batchsize))
+    n_valid_batches = pdsp_data.get_nbatches(batchsize) - startbatch
+    for batch, (X, y) in enumerate(
+        pdsp_data.get_training_batches(batchsize=batchsize, plane2_only=plane2, startbatch=startbatch)):
+      print(f'{batch}/{n_valid_batches}', end='\r')
+      n_valid += 1
+
+      pred = model(torch.from_numpy(X).float())
+      loss = loss_fn(pred, torch.from_numpy(y).float())
+      #loss, current = loss.item(), batch * len(X)
+      validation_losses[-1] += loss.item()
+    print(f'Epoch Validation Loss: {validation_losses[-1]/n_valid}')
--- process_hits.py
+++ process_hits.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import ceil
+from math import ceil, sqrt, exp, pi
 import h5py as h5
 from tensorflow.python.ops.numpy_ops import np_config
 import tensorflow as tf
@@ -9,13 +9,14 @@ import matplotlib.pyplot as plt
 np_config.enable_numpy_behavior()
 
 class PDSPData:
-  def __init__(self, filename, linked=False):
+  def __init__(self, filename, maxtime=913, linked=False):
     self.h5in = h5.File(filename)
     self.tp = np.dtype([('integral', 'f4'),
                         ('rms', 'f4'), ('time', 'f4'), ('wire', 'f4')])
 
     self.linked = linked
     self.loaded_truth = False
+    self.maxtime=maxtime
 
     if not linked:
       self.hit_events_0 = np.array(self.h5in['plane_0_hits']['event_id'])
@@ -32,6 +33,8 @@ class PDSPData:
       self.hit_events_1 = dict()
       self.hit_events_2 = dict()
 
+      self.k_nevents = dict()
+
       events = []
       for k in self.keys:
         self.hit_events_0[k] = np.array(self.h5in[f'{k}/plane_0_hits/event_id'])
@@ -39,7 +42,10 @@ class PDSPData:
         self.hit_events_2[k] = np.array(self.h5in[f'{k}/plane_2_hits/event_id'])
 
         events += [i for i in np.array(self.h5in[f'{k}/events/event_id'])]
-        self.event_keys += [k]*len(np.array(self.h5in[f'{k}/events/event_id']))
+        n_k_events = len(np.array(self.h5in[f'{k}/events/event_id']))
+        self.event_keys += [k]*n_k_events
+        self.k_nevents[k] = n_k_events
+        print(f'Added {n_k_events} events from {k}')
 
         nhits += [i for i in np.array(self.h5in[f'{k}/events/nhits'])]
       #print(self.event_keys)
@@ -90,16 +96,23 @@ class PDSPData:
       n_piplus = []
       n_piminus = []
       n_pi0 = []
+      self.k_ntruths = dict()
       for k in self.keys:
         if 'truth' in self.h5in[f'{k}'].keys():
           found_truth = True
-          pdg += [i for i in np.array(self.h5in[f'{k}/truth/pdg'])]
+
+          sub_pdg = [i for i in np.array(self.h5in[f'{k}/truth/pdg'])]
+
+          pdg += sub_pdg
           interacted += [i for i in np.array(self.h5in[f'{k}/truth/interacted'])]
           n_neutron += [i for i in np.array(self.h5in[f'{k}/truth/n_neutron'])]
           n_proton += [i for i in np.array(self.h5in[f'{k}/truth/n_proton'])]
           n_piplus += [i for i in np.array(self.h5in[f'{k}/truth/n_piplus'])]
           n_piminus += [i for i in np.array(self.h5in[f'{k}/truth/n_piminus'])]
           n_pi0 += [i for i in np.array(self.h5in[f'{k}/truth/n_pi0'])]
+
+          print(f'Added {len(sub_pdg)} truths from {k}')
+          self.k_ntruths[k] = len(sub_pdg)
 
       if found_truth:
         self.pdg = np.array(pdg)
@@ -149,7 +162,7 @@ class PDSPData:
 
     key = '' if not self.linked else f'{key}/'
 
-    print('Loading data from link', key)
+    #print('Loading data from link', key)
 
 
     for n in self.tp.names:
@@ -165,8 +178,8 @@ class PDSPData:
     self.plane2_data = self.load_data(2, eventindex=eventindex)
 
 
-  def make_plane(self, pid, pad2=False):
-    plane = np.zeros((913, 480 if (pid == 2 and not pad2) else 800))
+  def make_plane(self, pid, pad2=False, use_width=False):
+    plane = np.zeros((self.maxtime, 480 if (pid == 2 and not pad2) else 800))
 
     if pid not in [0, 1, 2]:
       ##TODO -- throw exception
@@ -175,16 +188,27 @@ class PDSPData:
     elif pid == 1: data = self.plane1_data
     elif pid == 2: data = self.plane2_data
 
-    for d in data:
 
+    bin_width=6.025
+    for d in data:
       w = int(d['wire'])
       if pid == 2:
         if w > 479: continue
         if pad2: w = w + (800 - 480)//2
       elif w > 799: continue
-      t = 912 - int((d['time'] - 500)/6.025)
+      t = 912 - int((d['time'] - 500)/bin_width)
+      if t >= self.maxtime: continue
       i = d['integral']
-      plane[t,w] += i
+
+      if not use_width:
+        plane[t,w] += i
+      else:
+        rms = d['rms']
+        #Get the number of bins away -- 5 sigma
+        nbins = ceil(5*rms/bin_width)
+        for b in range(t - nbins, t + nbins + 1):
+          if b >= self.maxtime: continue
+          plane[b,w] += i*(bin_width/(rms*sqrt(2.*pi)))*exp(-.5*((t - b)*bin_width/rms)**2)
   
     return plane
 
@@ -204,23 +228,33 @@ class PDSPData:
     self.event_keys = np.delete(self.event_keys, indices)
     self.nevents -= len(indices[0])
 
+  def get_nbatches(self, batchsize=2):
+    return ceil(self.nevents/batchsize)
 
-  def get_training_batches(self, batchsize=2):
+  def get_training_batches(self, batchsize=2, plane2_only=False, use_width=False, maxbatches=-1, startbatch=0):
 
-    for i in range(ceil(self.nevents/batchsize)):
+    for i in range(startbatch, ceil(self.nevents/batchsize)):
+      if maxbatches > 0 and i > maxbatches: break
       #print('Batch', i)
       #print('\t', np.arange(self.nevents)[i*batchsize:(i+1)*batchsize])
 
       batch_events = np.arange(self.nevents)[i*batchsize:(i+1)*batchsize]
       nb = len(batch_events)
-      plane_batch = np.zeros((nb, 3, 913, 800))
+      if plane2_only:
+        plane_batch = np.zeros((nb, 1, self.maxtime, 480))
+      else:
+        plane_batch = np.zeros((nb, 3, self.maxtime, 800))
+
       truth_batch = np.zeros((nb, 4))
       for a, j in enumerate(batch_events):
         #print(j)
         self.get_event(j)
-        plane_batch[a,0,:,:] = self.make_plane(0)
-        plane_batch[a,1,:,:] = self.make_plane(1)
-        plane_batch[a,2,:,160:-160] = self.make_plane(2)
+        if plane2_only:
+          plane_batch[a,0,:,:] = self.make_plane(2, use_width=True)
+        else:
+          plane_batch[a,0,:,:] = self.make_plane(0, use_width=True)
+          plane_batch[a,1,:,:] = self.make_plane(1, use_width=True)
+          plane_batch[a,2,:,160:-160] = self.make_plane(2, use_width=True)
 
         truth_batch[a,self.topos[j]] = 1.
 
